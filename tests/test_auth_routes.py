64c4f53f3b5420124ad5f2c7e292a0f7d3677740
Grant Ramsay <seapagan@gmail.com>
2023-05-05 13:29:45 +0100

    test more /register path edge cases
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- tests/test_auth_routes.py
+++ tests/test_auth_routes.py
@@ -4,36 +4,32 @@ import pytest
 from models.enums import RoleType
 from models.user import User
 
-
-def mock_template_send(self, backgroundtasks, email_data):
-    """Mock the template_send method."""
-    return None
+email_fn_to_patch = "managers.user.EmailManager.template_send"
+register_path = "/register"
 
 
 @pytest.mark.asyncio
 async def test_register_new_user(test_app, db, mocker):
     """Ensure a new user can register."""
-    # disable email sending
-    mocker.patch("managers.user.EmailManager.template_send", mock_template_send)
-
+    # disable email sending by mocking the function
     post_body = {
-        "email": "testuser@testing.com",
+        "email": "testuser@testuser.com",
         "first_name": "Test",
         "last_name": "User",
         "password": "test12345!",
     }
+    mock_send = mocker.patch(email_fn_to_patch)
     response = test_app.post(
-        "/register",
+        register_path,
         json=post_body,
     )
-
-    users_data = await db.fetch_one(User.select())
 
     assert response.status_code == 201
     assert list(response.json().keys()) == ["token", "refresh"]
     assert type(response.json()["token"]) is str
     assert type(response.json()["refresh"]) is str
 
+    users_data = await db.fetch_one(User.select())
     assert users_data["email"] == post_body["email"]
     assert users_data["first_name"] == post_body["first_name"]
     assert users_data["last_name"] == post_body["last_name"]
@@ -41,15 +37,17 @@ async def test_register_new_user(test_app, db, mocker):
     assert users_data["verified"] is False
     assert users_data["role"] == RoleType.user
 
+    mock_send.assert_called_once()
+
 
 @pytest.mark.asyncio
-async def test_register_new_user_bad_email(test_app, db, mocker):
-    """Ensure submitted email address is valid when registering."""
-    # disable email sending
-    mocker.patch("managers.user.EmailManager.template_send", mock_template_send)
+async def test_register_new_user_with_bad_email(test_app, db, mocker):
+    """Ensure an invalid email address fails, and no email is sent."""
+    # mock the email sending function
+    mock_send = mocker.patch(email_fn_to_patch)
 
     response = test_app.post(
-        "/register",
+        register_path,
         json={
             "email": "bad_email",
             "first_name": "Test",
@@ -58,8 +56,58 @@ async def test_register_new_user_bad_email(test_app, db, mocker):
         },
     )
 
+    assert response.status_code == 400
+    assert response.json()["detail"] == "This email address is not valid"
+
     users_data = await db.fetch_all(User.select())
     assert len(users_data) == 0
 
-    assert response.status_code == 400
-    assert response.json()["detail"] == "This email address is not valid"
+    mock_send.assert_not_called()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "post_body",
+    [
+        {},
+        {
+            "email": "",
+            "first_name": "Test",
+            "last_name": "User",
+            "password": "test12345!",
+        },
+        {
+            "email": "email@testuser.com",
+            "first_name": "",
+            "last_name": "User",
+            "password": "test12345!",
+        },
+        {
+            "email": "email@testuser.com",
+            "first_name": "Test",
+            "last_name": "",
+            "password": "test12345!",
+        },
+        {
+            "email": "email@testuser.com",
+            "first_name": "Test",
+            "last_name": "User",
+            "password": "",
+        },
+    ],
+)
+async def test_register_new_user_with_missing_data(
+    test_app, db, mocker, post_body
+):
+    """Ensure registering with missing data fails, and no email is sent."""
+    # mock the email sending function
+    mock_send = mocker.patch(email_fn_to_patch)
+
+    response = test_app.post(register_path, json=post_body)
+
+    assert response.status_code == 400 or 422
+
+    users_data = await db.fetch_all(User.select())
+    assert len(users_data) == 0
+
+    mock_send.assert_not_called()
