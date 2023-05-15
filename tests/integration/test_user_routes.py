6386b25d603acbf906a89b0dc12e3af8d1730af7
Grant Ramsay <seapagan@gmail.com>
2023-05-15 09:08:55 +0100

    test user routes blocked by auth
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- tests/integration/test_user_routes.py
+++ tests/integration/test_user_routes.py
@@ -1,1 +1,61 @@
 """Integration tests for user routes."""
+
+import pytest
+
+from managers.user import pwd_context
+
+test_user = {
+    "email": "testuser@usertest.com",
+    "first_name": "Test",
+    "last_name": "User",
+    "password": pwd_context.hash("test12345!"),
+    "verified": True,
+}
+
+
+@pytest.mark.parametrize(
+    "route",
+    [
+        ["/users", "get"],
+        ["/users/me", "get"],
+        ["/users/1/make-admin", "post"],
+        ["/users/1/password", "post"],
+        ["/users/1/ban", "post"],
+        ["/users/1/unban", "post"],
+        ["/users/1", "put"],
+        ["/users/1", "delete"],
+    ],
+)
+def test_routes_no_auth(test_app, route):
+    """Test that routes are protected by authentication."""
+    route_name, method = route
+    print(route_name, method)
+    fn = getattr(test_app, method)
+    response = fn(route_name)
+
+    print(response)
+    assert response.status_code == 403
+
+
+@pytest.mark.parametrize(
+    "route",
+    [
+        ["/users", "get"],
+        ["/users/me", "get"],
+        ["/users/1/make-admin", "post"],
+        ["/users/1/password", "post"],
+        ["/users/1/ban", "post"],
+        ["/users/1/unban", "post"],
+        ["/users/1", "put"],
+        ["/users/1", "delete"],
+    ],
+)
+def test_routes_bad_auth(test_app, route):
+    """Test that routes are protected by authentication."""
+    route_name, method = route
+    print(route_name, method)
+    fn = getattr(test_app, method)
+    response = fn(route_name, headers={"Authorization": "Bearer BADBEEF"})
+
+    print(response)
+    assert response.status_code == 401
